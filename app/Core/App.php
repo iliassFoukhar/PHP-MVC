5b4176bcc878a19b05db16bf956df086e324148b
Iliass Foukhar <nextgendevelopement@gmail.com>
2022-02-01 16:31:57 +0100

    Parsing urls + htaccess files

--- app/Core/App.php
+++ app/Core/App.php
@@ -1,7 +1,18 @@
 <?php
 
 class App{
+    protected $controller = 'home';
+    protected $method = "index";
+    protected $params = [];
 
+    public function __construct(){
+        $url = $this->parseURL();
+    }
+
+    protected function parseURL(){
+        if(isset($_GET["url"])){
+            return $url = explode('/', filter_var(rtrim($_GET["url"], '/'), FILTER_SANITIZE_URL));
+        }
+    }
 }
-
 ?>
