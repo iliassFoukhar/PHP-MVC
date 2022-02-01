8861406e9991a1a71b09d759f2d63cdb22d269ad
Iliass Foukhar <nextgendevelopement@gmail.com>
2022-02-01 17:05:33 +0100

    Controllers core setup

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,8 +1,13 @@
 <?php 
     class Home extends Controller{
 
-        public function index(){
+        public function index($param, $param2){
             echo "Home Index OK!!";
+            
+        }
+
+        public function foo(){
+            echo "Foo";
         }
     }
 ?>
